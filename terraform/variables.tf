d13d800f2ae40035d13aec7380077481a1e303f0
nicholaschangIT <nicholasc001@hotmail.com>
2024-09-22 17:53:25 +0100

    remove pat token variable

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -13,11 +13,6 @@ variable "azure_devops_org_service_url" {
   type = string
 }
 
-variable "azure_devops_pat" {
-  type = string 
-  sensitive = true
-}
-
 variable "subscription_id" {
   type = string
 }
